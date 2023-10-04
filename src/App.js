df28a03c23652bb31c59efdeb082f1073c5ebb59
Sofía González <sofia_1.6@hotmail.com>
2023-10-04 20:49:29 -0300

    updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"; 
+
+import ErrorPage from "./pages/Error";
+import HomePage from "./pages/Home";
+import ProductsPage from "./pages/Products";
+import RootLayout from "./pages/Root";
+
+const router = createBrowserRouter([
+  {
+    path: '/', 
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { path: '/', element: <HomePage /> },
+      { path: '/products', element: <ProductsPage /> },
+    ]
+  },
+  
+]);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
